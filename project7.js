b6bf5a3ed9998e151c9fff85fb933c6a6b089707
Harsha4576dks <harshivahm@gmail.com>
2026-08-31 19:07:08 +0530

    examples

--- project7.js
+++ project7.js
@@ -1,7 +1,23 @@
-async function get_Joke(){
+/* async function get_Joke(){
     const response = await fetch("https://official-joke-api.appspot.com/random_joke");
     const data = await response.json();
     console.log(`${data.setup} - ${data.punchline}`);
 
 }
-get_Joke();
+get_Joke(); 
+
+async function intro(){
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data  = await response.json();
+    console.log(data);    
+}
+intro();
+*/
+
+async function animal(){
+    const response = await fetch("https://en.wikipedia.org/api/rest_v1/page/summary/Elephant");
+    const mammal = await response.json();
+    console.log(mammal);
+}
+
+animal();
